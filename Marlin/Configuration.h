ad8ab3fb3ade8eb27454277a14c068bff9eca014
redleader36 <redleader36@gmail.com>
2021-02-02 22:55:41 -0600

    Removed Sensorless Homing
    Removed Sensorless Homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -654,8 +654,8 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
+#define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 397, 153 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 397, 90 }
 
 /**
  * Default Max Feed Rate (mm/s)
